4d90a00ea3eb216c15a6293454edc895a049414a
alexmcdermid <alexander.mcdermid@gmail.com>
2021-10-22 12:08:42 -0700

    very basic django webapp created with static homepage

--- tigerpins/tigerpins/settings.py
+++ tigerpins/tigerpins/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mainapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'tigerpins.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tigerpins',
     }
 }
 
