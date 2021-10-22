25b2d8692b2dde99a09dd3f6e151d97e09a4b219
alexmcdermid <alexander.mcdermid@gmail.com>
2021-10-22 12:33:40 -0700

    add many to many model for location and user

--- tigerpins/mainapp/models.py
+++ tigerpins/mainapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import IntegerField
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Locations(models.Model):
+    locationName = models.CharField(max_length=100)
+    lat = models.FloatField()
+    long = models.FloatField()
+
+    user = models.ManyToManyField(User)
